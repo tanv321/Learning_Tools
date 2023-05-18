ebc7fbf172e7d1d8de7625960a49c09f69cf1ae2
tanv321 <tanvalam321@gmail.com>
2023-05-18 14:44:52 -0400

    setting up environment for exexercise2

--- React/react_exercises/src/Routes.js
+++ React/react_exercises/src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Exercise1 from './components/hooks/exercise1';
 
+import Exercise2 from './components/hooks/exercise2';
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +12,7 @@ function App() {
         <Routes>
         /* navigate to localhost:3000/hooks/exercise1   */
           <Route path="/hooks/exercise1" element={<Exercise1 />} /> 
-          {/* Add more routes for other exercises */}
+          <Route path="/hooks/exercise2" element={<Exercise2/>} />
         </Routes>
       </Router>
     </div>
---
+++ React/react_exercises/src/components/hooks/exercise2.js
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Exercise2 () {
+    return (
+        <h1 style= {{fontSize : '18px' }} >
+            Create a React component called "TodoList". This component should render a list of todo items. Each todo item should have a checkbox and a label displaying the task description. When a checkbox is checked, the corresponding todo item should be marked as completed and displayed with a line-through style. Additionally, the component should have an input field and a button that allows the user to add new todo items to the list.
+        </h1>
+    );
+}
