c31e5a16f899880acded02ec922035086e028c27
tanv321 <tanvalam321@gmail.com>
2023-05-20 11:43:14 -0400

    skeleton code for exercise 4

--- React/react_exercises/src/components/hooks/exercise4.js
+++ React/react_exercises/src/components/hooks/exercise4.js
@@ -3,6 +3,16 @@ import react, {useState} from 'react';
 import { HorizontalBarrier } from './sharedComponents';
 
 export default function Exercise4() {
+    const [displayedNumber, setDisplayedNumber] = useState('');
+    const [result, setResult] = useState('');
+
+    const btnValues = [
+        ["C", "+-", "%", "/"],
+        [7, 8, 9, "X"],
+        [4, 5, 6, "-"],
+        [1, 2, 3, "+"],
+        [0, ".", "cos","="],
+      ];
 
     return (
         <div>
@@ -16,7 +26,21 @@ export default function Exercise4() {
          on the screen when the "=" button is clicked.
       </h1>
       <HorizontalBarrier />
+       <h1>what is this</h1>
+       {btnValues.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((value, columnIndex) => (
+            <button key={columnIndex}>{value}</button>
+            
+          ))}
+        </div>
+      ))}
+
+
+
+
       </div>
+    
     )
 
 }
