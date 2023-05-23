bd8127b1d4dd9f684b2c0e657222848fefccd347
tanv321 <tanvalam321@gmail.com>
2023-05-23 13:21:39 -0400

    able to register simple interaction; todo: division

--- React/react_exercises/src/components/hooks/exercise4.js
+++ React/react_exercises/src/components/hooks/exercise4.js
@@ -1,29 +1,63 @@
 import react, {useState} from 'react';
 
 import { HorizontalBarrier } from './sharedComponents';
+import { type } from '@testing-library/user-event/dist/type';
 
 export default function Exercise4() {
-    const [displayedNumber, setDisplayedNumber] = useState('');
-    const [result, setResult] = useState('');
+    let [displayedNumber, setDisplayedNumber] = useState('');
+    let [displayedNumber2, setDisplayedNumber2] = useState('');
+    let [result, setResult] = useState(0);
+    let [operator, setOperator] = useState(null);
 
     const btnValues = [
         ["A", "B", "C", "/"],
-        [7, 8, 9, "X"],
+        [7, 8, 9, "*"],
         [4, 5, 6, "-"],
         [1, 2, 3, "+"],
         [0, "D", "E","="],
       ];
     
+     /* user select numbers we store that number in displayedNumber
+           user select operator we store that in setOperator 
+           user select number we look at setOperator and perform with respect to the operator then make setoperator empty
+           user select "C" we clear the result*/
     const valueCapture = (value) => {
       const value_clicked = value.target.textContent.trim()
 
       if (value_clicked >= 0 && value_clicked <=9){
-        console.log(value_clicked);
-        setDisplayedNumber(value_clicked)
-
-      } else if (value_clicked === "+"){
         
+          if (operator != null){
+            console.log("2displayednumber2+", displayedNumber2,"<---displayed number2")
+            setDisplayedNumber2(displayedNumber2+value_clicked)
+            
+
+          } else {
+            setDisplayedNumber(displayedNumber+value_clicked);
+            console.log("displayednumber", displayedNumber);
+          }
+
+      } else {
+        console.log("operator is selected", value_clicked)
+        if (value_clicked != "="){
+          setOperator(value_clicked);
+        } else {
+          if (operator != null){
+          
+          let to_process = eval(`${displayedNumber}${operator[0]}${displayedNumber2}`);
+
+          setResult(to_process);
+          setOperator(null);
+          setDisplayedNumber('');
+          setDisplayedNumber2('')
+          console.log("final", result, displayedNumber, operator);
+          
+          }
+        }
+        
+
       }
+      
+      console.log("everything", result, displayedNumber, operator);
       
     }
     return (
