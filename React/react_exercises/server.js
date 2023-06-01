a57ae1765d7988bb4199d358d0e565aeb9a2af9e
tanv321 <tanvalam321@gmail.com>
2023-06-01 11:19:47 -0400

    sorted server.js code

--- React/react_exercises/server.js
+++ React/react_exercises/server.js
@@ -1,42 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
-dotenv.config({ path: 'C:/Users/alamt/Documents/Programming_Projects/.env'});
-
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
+
+dotenv.config({ path: 'C:/Users/alamt/Documents/Programming_Projects/.env'});
 let x = process.env.API_KEY
-console.log(x);
+
 
 app.get('/', (req, res) => {
     res.send('Hello from the root path of the server side!');
   });
 
-app.get('/api/data', (req, res) => {
-    // Handle the request and send a response
-    const data = {
-      message: 'Hello from the server!',
-    };
-    res.json(data);
-  });
 
 app.post('/locationName', (req, res) => {
-
   const data = req.body;
   let infoRetrieve = "http://api.weatherapi.com/v1/current.json?key="+x+"&q=";
-        
   infoRetrieve+=data.key
-        
   const link = {
     message: infoRetrieve,
   };
   res.json(link);
-  
+
 })
 
 
