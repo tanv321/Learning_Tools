38b65fcb70c6c73bdb943ab83e540b98215e0051
tanv321 <tanvalam321@gmail.com>
2023-06-07 15:28:05 -0400

    expanded on exercise8

--- React/react_exercises/src/components/hooks/exercise8.js
+++ React/react_exercises/src/components/hooks/exercise8.js
@@ -3,14 +3,22 @@ import { HorizontalBarrier } from './sharedComponents';
 
 export default function Exercise8() {
     const [isShow, setIsShow] = useState(false);
+    const [isShowQ2, setIsShowQ2] = useState(false);
 
     const toggleCollapse = () => {
       setIsShow(true)
 
     }
 
+    const toggleCollapseQ2 = () => {
+        setIsShowQ2(true)
+  
+      }
     const collapseOn = () => {
         setIsShow(false);
+    }
+    const collapseOnQ2 = () => {
+        setIsShowQ2(false);
     }
 
     return (
@@ -24,10 +32,24 @@ export default function Exercise8() {
         and state management in React.
         </h1>
         <HorizontalBarrier />
+        <h1 style={{ borderBottom: "2px solid #fff ", marginLeft: 20, marginRight: 20 }}> FAQ </h1>
+        
+        <p style={{ fontSize:"13px"}}> What is React? React is a JavaScript library for building user interfaces.</p>
+        {isShow === true && ( <p style={{ fontSize:"13px"}}>
+         It allows developers to create reusable UI components and build complex UIs by composing these components together. 
+        React follows a component-based architecture and uses a virtual DOM (Document Object Model) for efficient updates and rendering.
+         It is widely used for building modern web applications and provides a declarative and efficient way to handle UI updates.  <button onClick={collapseOn}> hide detail</button></p> )} 
+        {isShow === false && <button onClick={toggleCollapse}> show more detail </button> }
 
-        <p style={{ fontSize:"13px"}}> This is an example text which is</p>
-        {isShow === true && ( <p style={{ fontSize:"13px"}}>random secret information  <button onClick={collapseOn}> hide detail</button></p> )} 
-        {isShow === false && <button onClick={toggleCollapse}> show detail </button> }
+
+        <p style={{ fontSize:"13px", borderTop: "2px solid #fff ", marginLeft: 20, marginRight: 20 }}> Is React a framework or a library? React is a JavaScript library for building user interfaces.</p>
+        {isShowQ2 === true && ( <p style={{ fontSize:"13px"}}>
+        React is often referred to as a library because it focuses on the view layer of an application. 
+        However, with the ecosystem of supporting libraries and tools, such as React Router for routing and Redux for state management, 
+        React can be used as a foundation to build full-fledged web applications. 
+        While it lacks some features typically found in comprehensive frameworks, React's modular and flexible nature allows developers to choose and integrate additional libraries as needed.  <button onClick={collapseOnQ2}> hide detail</button></p> )} 
+        {isShowQ2 === false && <button onClick={toggleCollapseQ2}> show more detail </button> }
+        
         
         </div>
     );
