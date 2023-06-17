3d3637da620f533c494af8316d82315835138f86
tanv321 <tanvalam321@gmail.com>
2023-06-17 15:10:19 -0400

    added functionality where users can select day and month

--- React/react_exercises/src/components/hooks/exercise28.js
+++ React/react_exercises/src/components/hooks/exercise28.js
@@ -3,16 +3,41 @@ import { HorizontalBarrier } from './sharedComponents';
 
 export default function Exercise28(){
     const [appointment, setAppointment] = useState(false)
-    
+    const [months, setMonths] = useState([])
+    const [showDate, setShowDate] = useState(false)
+
 
     const showDates = () => {
-        console.log("we clicked");
         setAppointment(true)
     }
 
 
+
+    const calender = [
+        ["Jan", "Feb", "Mar", "Apr"],
+        ["May", "Jun", "Jul", "Aug"],
+        ["Sep", "Oct", "Nov", "Dec"],
+      ];
+    
+    const days = [
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+        ["21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+      ];
+
+    const monthPicked = (value) => {
+        setShowDate(true);
+        console.log(value.target.textContent)
+
+    }
+    const daysPicked = (value) => {
+        console.log(value.target.textContent)
+
+    }
+
     return (
         <div>
+
         <h1> Exercise Twenty-eight</h1>
         <h1 style={{ fontSize: '10px' }}>
         Create a React component called "BookingCalendar". 
@@ -22,7 +47,41 @@ export default function Exercise28(){
         <HorizontalBarrier />
         <p style={{ fontSize: '13px' }} > Click <button onClick= { showDates } > "need appointment" </button>, to see availabilities. </p>
 
-        {appointment == true && <button> this should show the calender when user clicks on "need appointment" </button>}    
+        { appointment == true && (
+        <div>
+            <p> Months </p> { calender.map((row, rowIndex) => (
+        
+        <div key={rowIndex}>
+          {row.map((value, columnIndex) => (
+            <button key={columnIndex}
+            onClick={ monthPicked }>  {value}
+            </button>
+            
+          ))}
+          </div>
+        
+        ))}
+        </div>
+      )}
+
+
+        { showDate == true && (
+        <div>
+            <p> Days </p> { days.map((row, rowIndex) => (
+
+        <div key={rowIndex}>
+          {row.map((value, columnIndex) => (
+            <button key={columnIndex}
+            onClick={ daysPicked }>  {value}
+            </button>
+            
+          ))}
+          </div>
+        
+        ))}
+        </div>
+      )}
+        
        
         
         
