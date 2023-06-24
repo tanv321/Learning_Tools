eea7de2acb81c4ade6a190efbb0b7891b2e76c17
tanv321 <tanvalam321@gmail.com>
2023-06-24 10:51:28 -0400

    shows the amount user entered; evaluate and give feedback to users answer on submit; todo:fix styling

--- React/react_exercises/src/components/hooks/exercise36.js
+++ React/react_exercises/src/components/hooks/exercise36.js
@@ -12,16 +12,22 @@ export default function Exercise36() {
         1: 0, 5: 0, 10: 0, 20:0, 50: 0, 100: 0,
        0.01: 0, 0.05: 0, 0.10: 0, 0.25: 0,
    });
-
+    let [compute, setCompute] = useState(0.00);
     let [charged, setCharged] = useState("")
     let [customerGives, setCustomerGives] = useState("");
+    let [result, setResult] = useState("");
 
     
 
     const startService = () => {
 
+        setUserSelected({
+            1: 0, 5: 0, 10: 0, 20:0, 50: 0, 100: 0,
+            0.01: 0, 0.05: 0, 0.10: 0, 0.25: 0,
+          });
+
         const chargedFloat = parseFloat((Math.random() * 20 + 1).toFixed(2));
-    const customerGivesFloat = parseFloat((Math.random() * (100 - chargedFloat) + chargedFloat).toFixed(2));
+        const customerGivesFloat = parseFloat((Math.random() * (100 - chargedFloat) + chargedFloat).toFixed(2));
     
         setCharged(chargedFloat);
         setCustomerGives(customerGivesFloat);
@@ -53,11 +59,21 @@ export default function Exercise36() {
     };
 
 
+
+    const clearAnswer = () => {
+        setUserSelected({
+            1: 0, 5: 0, 10: 0, 20:0, 50: 0, 100: 0,
+            0.01: 0, 0.05: 0, 0.10: 0, 0.25: 0,
+          });
+
+        setCompute(0.00);
+
+    }
     const evalAnswer = () => {
         if (isEqual(toGive, userSelected)) {
-            console.log("Correct")
+            setResult("You got the Correct Answer!")
         }  else {
-            console.log("wrong check back")
+            setResult("Wrong answer...")
             console.log(toGive, userSelected);
         }
     }
@@ -94,7 +110,8 @@ export default function Exercise36() {
         
         if (value_clicked.startsWith("$")) {
             trimmedValue = value_clicked.substr(1, value_clicked.length-1);
-            console.log(trimmedValue, "<----thiss is trimmedvalue");
+            setCompute(prevState => prevState + parseFloat(trimmedValue));
+            // console.log(trimmedValue, "<----thiss is trimmedvalue", compute, typeof(trimmedValue));
         } else {
             trimmedValue = value_clicked.substr(0, value_clicked.length-6);
             switch (trimmedValue) {
@@ -111,9 +128,12 @@ export default function Exercise36() {
                     trimmedValue = "0.25";
                     break;
                 default:
-                    trimmedValue = 99999;
+                    console.log(" error ")
+                    break;
             }
-            console.log(trimmedValue, "hmm", trimmedValue.length, trimmedValue===1);
+            setCompute(prevState => parseFloat((prevState + parseFloat(trimmedValue)).toFixed(2)));
+
+            console.log(trimmedValue, "hmm", trimmedValue.length, trimmedValue===1, compute);
 
         }
     
@@ -131,13 +151,17 @@ export default function Exercise36() {
         console.log(value_clicked, userSelected);
 
         }
+
+        
         
       
       
       
     
     return (
+        
         <div>
+
       <h1>Exercise Thirty-six</h1>
       <h1 style={{ fontSize: '10px' }}>
       Create a React component called "CashRegister". 
@@ -183,15 +207,16 @@ export default function Exercise36() {
     <div style={{ fontSize:"20px" }}>
             {Object.entries(userSelected).map(([key, value]) => (
                 <div key={key}>
-                    Changes: {"$"}{key}, Count: {value}
+                    {"$"}{key} = Count: {value}{" "}
                 </div>
             ))}
         </div>
 
     <button style={{ color:"brown", fontSize:'70px' }} onClick={evalAnswer}> submit </button>
-
+    <p style={{ color:"pink", fontSize:"30px"}}>{result}</p>
     
-
+    <p style={{fontSize:"20px", color:"green"}} > Your Current balance adds upto ${compute} </p>
+    <button style={{ color:"red", fontSize:'20px' }} onClick={clearAnswer}> reset your answer </button>
 
       </div>
     
